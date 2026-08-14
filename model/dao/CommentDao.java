faadfe377d0e9a030ded8c59ea8310e54215671c
seungy07 <ksykim07@gmail.com>
2026-08-14 16:33:22 +0900

    메인함수 반영

--- model/dao/CommentDao.java
+++ model/dao/CommentDao.java
@@ -1,10 +1,36 @@
 package model.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.dto.CommentDto;
+
+
 public class CommentDao {
     private CommentDao(){}
     private static final CommentDao instance = new CommentDao();
     public static CommentDao getInstance(){return instance;}
 
-    
+    // 등록
+    public boolean c_save(CommentDto commentdto){
+        try{
+            String sql = "insert into comment(b_no, c_content, c_writer) values(?, ?, ?) ";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt( 1, commentdto.getB_no());
+            ps.setString(2, commentdto.getC_content());
+            ps.setString(3, commentdto.getC_writer());
+            int count = ps.executeUpdate();
+            if(count >= 1){ return true; }
+        }catch(SQLException e){ System.out.println(e); }
+        return false;
+    }
+   
+    // 출력
+
+    // 수정
+
+    // 삭제
+
+
     
 }
--- view/CommentView.java
+++ view/CommentView.java
@@ -1,6 +1,10 @@
 package view;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import controlller.CommentController;
+import model.dto.CommentDto;
 
 public class CommentView {
     private CommentView(){}
@@ -8,6 +12,45 @@ public class CommentView {
     public static CommentView getInstance(){return instance;}
     
     private CommentController cc = CommentController.getInstance();
+
+    private Scanner scan = new Scanner(System.in);
+
+    // 최초 실행 화면
+    public void c_run(){
+        while( true ){
+            try{ 
+                System.out.println("================댓글============= ");
+                System.out.println("1.등록 | 2.조회 | 3. 수정 | 4.삭제 ");
+                System.out.println("================================= ");
+                System.out.print("번호 선택> ");
+                int c_ch = scan.nextInt();
+                if(c_ch==1){c_save();}
+                else if(c_ch==2){}
+                else if(c_ch==3){}
+                else if(c_ch==4){}
+            }catch( InputMismatchException e){
+                scan = new Scanner(System.in);
+                System.out.println("정수만 입력" + e);
+            }
+        }
+    }
+
+    // 등록
+    public void c_save(){
+        System.out.print("댓글 작성할 게시물 번호: ");  int b_no = scan.nextInt();
+        System.out.print("댓글 내용: "); String c_content = scan.next();
+        System.out.print("댓글 작성자: "); String c_writer = scan.next();
+        CommentDto commentdto = new CommentDto( b_no, 0 ,c_content,c_writer);
+        boolean r = cc.c_save(commentdto);
+        if(r){System.out.println("댓글 등록 성공.");}else{System.out.println("댓글 등록실패.");}
+    }
+
+    // 조회 
+    
+    
+    // 수정
+
+    // 삭제
 
 
 
