17fa57340657cf55c72072d9516a4eb2dc4ebd0a
yoohyun0830-ship-it <yoohyun0830@gmail.com>
2026-08-14 16:42:00 +0900

    BoardController Code 완성

--- controlller/BoardController.java
+++ controlller/BoardController.java
@@ -1,6 +1,9 @@
 package controlller;
 
+import java.util.ArrayList;
+
 import model.dao.BoardDao;
+import model.dto.BoardDto;
 
 public class BoardController {
     private BoardController(){}
@@ -8,5 +11,21 @@ public class BoardController {
     public static BoardController getInstance(){ return instance;}
     private BoardDao bd = BoardDao.getInstance();  
 
-    public boolean b_save
+    public boolean b_save(BoardDto boardDto){
+        boolean result = bd.b_save(boardDto);
+        return result;
+    }
+
+    public ArrayList<BoardDto> b_findAll(){
+        ArrayList<BoardDto> result = bd.b_findAll();
+        return result;
+    }
+
+    public boolean b_update(BoardDto boardDto){
+        return bd.b_update(boardDto);
+    }
+
+    public boolean b_delete(int b_no ){
+        return bd.b_delete(b_no);
+    }
 }
